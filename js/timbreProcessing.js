ad25e4dd676c28e2de12f5bb429c41da2d3e398e
Nat <bnat1@umbc.edu>
2016-10-01 15:25:03 -0400

    working on creating sound objects when users updated

--- js/timbreProcessing.js
+++ js/timbreProcessing.js
@@ -1,3 +1,20 @@
 /**
  * Created by cferrier on 10/1/2016.
  */
+
+// members
+for (var username in members){
+	if oldMembers.hasOwnProperty(username){
+		// username:instrument
+		if (oldMembers[username] != newMembers[username]){
+
+		}
+	}
+}
+	
+
+// set instrument
+// set frequency
+// set volume
+
+//play 
