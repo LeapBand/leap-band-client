f535f80601c1adeb9fcebb37590d4297a089a78f
Nat <bnat1@umbc.edu>
2016-10-01 14:54:56 -0400

    network.js

--- js/network.js
+++ js/network.js
@@ -1,3 +1,27 @@
 /**
  * Created by cferrier on 10/1/2016.
+ *
+ * 
  */
+var socket = io('http://localhost:3000');
+
+// client instrument dictionary
+
+var members = {};
+// sending info
+
+//use this when changing instrument
+socket.emit('update_member',{username:username, instrument:inst});
+
+//whatever client needs to play music
+socket.emit('play',{});
+
+// taking info
+socket.on('play', function(data) {
+	console.log(data);
+});
+
+// update the members
+socket.on('members_update', function(data) {
+	members = data;
+});
