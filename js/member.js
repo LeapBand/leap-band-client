de23218d0a94a83eab3cb83849475880bef657b6
Eric Hebert <ayebear@gmail.com>
2016-10-02 01:45:56 -0400

    Swiping left/right with both hands now changes your instruments

--- js/member.js
+++ js/member.js
@@ -2,22 +2,23 @@ class Member {
 	constructor(socket, data) {
 		this.socket = socket;
 		this.instruments = {
-			drums: Drums
-			// piano: Piano,
+			drums: Drums,
+			piano: Piano
 			// guitar: Guitar,
+			// techno: Techno
 		};
 
 		this.swapCooldown = false;
 
-		this.instrumentOrder = [
+		this.instrumentNames = [
 			'drums',
-			'piano',
-			'guitar'
+			'piano'
+			// 'guitar',
+			// 'techno'
 		];
+		this.instrumentId = 0;
 
 		// this.username = '';
-		// this.instrumentName = '';
-		// this.instrument = null;
 		this.memberData = data;
 	}
 
@@ -26,14 +27,13 @@ class Member {
 		if (frame.hands.length == 2 && !this.swapCooldown)
 		{
 			if (Math.abs(frame.hands[0].palmPosition[0] - frame.hands[1].palmPosition[0])< 150){
-				console.log("Trigger 1");
-
 				if (frame.hands[0].palmVelocity[0] > 800 && frame.hands[0].palmVelocity[0] > 800){
 					console.log("Swiped right");
 					this.toggleCooldown();
 					setTimeout(() => {
 						this.toggleCooldown();
 					}, 250);
+					this.changeInstrumentFromDelta(1);
 				}
 
 				if (frame.hands[0].palmVelocity[0] < -800 && frame.hands[0].palmVelocity[0] < -800){
@@ -42,6 +42,7 @@ class Member {
 					setTimeout(() => {
 						this.toggleCooldown();
 					}, 250);
+					this.changeInstrumentFromDelta(-1);
 				}
 			}
 		}
@@ -64,6 +65,7 @@ class Member {
 			console.log('Created instrument: ' + instrumentName);
 			this.memberData.instrument = new this.instruments[instrumentName](socket);
 			this.memberData.instrument.name = instrumentName;
+			this.memberData.instrumentName = instrumentName;
 			return true;
 		}
 		return false;
@@ -75,6 +77,26 @@ class Member {
 
 	toggleCooldown() {
 		this.swapCooldown = !this.swapCooldown;
+	}
+
+	// Delta can be something like -1 or 1
+	changeInstrumentFromDelta(delta) {
+		this.instrumentId += delta;
+		if (this.instrumentId < 0) {
+			this.instrumentId += this.instrumentNames.length;
+		}
+		this.instrumentId %= this.instrumentNames.length;
+
+		let newName = this.instrumentNames[this.instrumentId];
+
+		console.log('instrumentId: ' + this.instrumentId);
+		console.log('instrumentName: ' + newName);
+
+		// Update actual instrument name on the server
+		this.socket.emit('update_member', {
+			username: 'testuser',
+			instrumentName: newName
+		});
 	}
 }
 
